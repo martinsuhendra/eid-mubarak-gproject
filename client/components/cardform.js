648e250f4f5a24ae4369da330743e3b0a6da0a89
noviirna <novi.irnawati@gmail.com>
2019-05-10 15:21:42 +0700

    upload image to gcs

--- client/components/cardform.js
+++ client/components/cardform.js
@@ -51,16 +51,56 @@ Vue.component('input-form', {
   `
 })
 
-Vue.component('result', {
-  props : [],
-  data : function(){
-    return {
-      to : "",
-      text : "",
-      url : ""
-    }
-  },
-  template : `
-  
-  `
-})
+// Vue.component('input-form', {
+//   props : [],
+//   data : function(){
+//     return {
+//       recipient : "",
+//       message : "",
+//       image : ""
+//     }
+//   },
+//   methods : {
+//     previewFile : function(event){
+//       this.image = event.target.files[0]    
+        
+//     },
+//     submitInput : function(){
+//       if(this.recipient == "" || this.message == "" ){
+//         console.log("dapet")
+//         swal("info", "complete the form below to make your card", "info");
+        
+//       } else {
+//         console.log("disini")
+//         console.log(this.recipient, this.message)
+//         this.$emit('newcard', { to : this.recipient, text : this.message, image : this.image })
+//         this.recipient = ""
+//         this.message = ""
+//         this.image = ""
+//         let submit = document.getElementById("submitbg")
+//         submit.value = ""
+//         if(!/safari/i.test(navigator.userAgent)){
+//           submit.type = ''
+//           submit.type = 'file'
+//         }
+//       }
+//     }
+//   },
+//   template : `
+//   <form class="form" @submit.prevent="submitInput">
+//     <div class="form-group">
+//       <label >Recipient Name</label>
+//       <input type="text" class="form-control" v-model="recipient" placeholder="Enter recipient name">
+//     </div>
+//     <div class="form-group">
+//       <label >Message</label>
+//       <input type="text" class="form-control" v-model="message" placeholder="Enter your message">
+//     </div>
+//     <div class="form-group">
+//       <label>Upload a picture</label>
+//       <input type="file" @change.prevent="previewFile" id="submitbg" class="form-control-file">
+//     </div>
+//     <button type="submit" class="btn btn-primary">Submit</button>
+//   </form>
+//   `
+// })
