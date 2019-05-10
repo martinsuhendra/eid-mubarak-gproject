e4f4c60a137d861f36edcb7b37318cd73bba9a01
emrido <dodooridoo@gmail.com>
2019-05-10 13:56:37 +0700

    basic custom styling

--- client/js/script.js
+++ client/js/script.js
@@ -10,13 +10,21 @@ var app = new Vue({
     email: "",
     password: "",
     isLogin: "",
-    page: "home"
+    page: "home",
+    top: -18,
+    right: 0,
+    width: 300,
+    height: 200,
+    fontStyle: "'Aref Ruqaa', serif",
+    fontSize: 16,
+    text: '',
+    textColor: '#000000'
   },
   methods: {
     generatecard(el) {
       var file = el.image;
       var reader = new FileReader();
-      reader.onloadend = function() {
+      reader.onloadend = function () {
         $("#canvas").css("background-image", 'url("' + reader.result + '")');
       };
       if (file) {
